3faa58be7cf4ce7e718fd0972532c4095fd19fef
Ipjak <sciencerhgkr@gmail.com>
2021-11-04 17:57:12 +0900

    fragment 구현

--- app/src/main/java/com/cookandroid/graduationproject/MainActivity.java
+++ app/src/main/java/com/cookandroid/graduationproject/MainActivity.java
@@ -1,30 +1,73 @@
 package com.cookandroid.graduationproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private FragmentGraph fragmentGraph = new FragmentGraph();
+    private FragmentHome fragmentHome = new FragmentHome();
+    private FragmentSetting fragmentSetting = new FragmentSetting();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //로딩화면
         Intent intent = new Intent(this, LoadingActivity.class);
         startActivity(intent);
 
-        Button imageButton = (Button) findViewById(R.id.button);
-        imageButton.setOnClickListener(new View.OnClickListener() {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragmentHome).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+
+        //        //버튼누르면
+        Button button = (Button) findViewById(R.id.temporaryButton);
+        button.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), LogIn.class);
                 startActivity(intent);
             }
+        });
+    }
 
-    });
-}
+
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch(menuItem.getItemId())
+            {
+                case R.id.graphItem:Item:
+                    transaction.replace(R.id.frameLayout, fragmentGraph).commitAllowingStateLoss();
+
+                    break;
+                case R.id.homeItem:Item:
+                    transaction.replace(R.id.frameLayout, fragmentHome).commitAllowingStateLoss();
+                    break;
+                case R.id.settingItem:Item:
+                    transaction.replace(R.id.frameLayout, fragmentSetting).commitAllowingStateLoss();
+                    break;
+            }
+            return true;
+        }
+    }
+
+
 }
